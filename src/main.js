516beb263f026a6b995af662e05f118a9e6e971c
Jakob Csörgei Gustavsson <soulrivers@gmail.com>
2017-04-03 23:45:28 +0200

    C:/Program Files/Git/rec command replays the next voice

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const auth = require('./auth.json');
+const fs = require('fs');
 
 client.on('ready', () =>  {
 	console.log("I am ready!");
@@ -33,10 +34,11 @@ client.on('message', m => {
 						console.log('Connected to channel ' + channelToJoin.name);
 						conns.push(connection);
 					})
-					.catch(connection => console.log('Unable to connect to channel ' + channelToJoin.name));
+						.catch(connection => console.log('Unable to connect to channel ' + channelToJoin.name));
 			}
 		}
 	}
+
 	if(m.content.startsWith('/leave')) {
 		if(conns !== undefined && conns[0] !== undefined) {
 			conns[0].disconnect();
@@ -46,4 +48,35 @@ client.on('message', m => {
 			console.log('Not connected to any channel');
 		}
 	}
+	if(m.content.startsWith('/rec')) {
+		//console.log(conns[0].channel.speakable);
+		// play streams using ytdl-core
+		//playYoutube();
+
+		recNextVoice();
+	}
 });
+// 163947791729557504
+// 
+let streams = [];
+function recNextVoice() {
+    if (conns !== undefined && conns[0] !== undefined) {
+        receiver = conns[0].createReceiver();
+        conns[0].on('speaking', (user, speaking) => {
+            if (speaking) {
+                stream = receiver.createPCMStream("163947791729557504");
+                streams.push(stream);
+            } else {
+                conns[0].playConvertedStream(streams.pop());
+            }
+        });
+    }
+}
+
+function playYoutube() {
+	const ytdl = require('ytdl-core');
+	const streamOptions = { seek: 0, volume: 1 };
+    const stream = ytdl('https://www.youtube.com/watch?v=XAWgeLF9EVQ', {filter : 'audioonly'});
+  	console.log(stream);
+    const dispatcher = conns[0].playStream(stream, streamOptions);
+}
