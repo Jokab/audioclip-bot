1a282fd3518b48d9c68aa720504a2dfcee450a7e
Jakob Csörgei Gustavsson <soulrivers@gmail.com>
2017-04-20 17:21:23 +0200

    Return file url in promise

--- src/aws.js
+++ src/aws.js
@@ -7,18 +7,29 @@ const fs = require('fs');
 function upload(file) {
 	var s3 = new AWS.S3({signatureVersion: 'v4'});
 
-	fs.readFile(file, function(err,data) {
-		var params = {
-			Bucket: 'audioclip-bot', 
-			Key: 'test.wav', 
-			Body: data,
-			ACL: 'public-read',
-			ContentType: 'audio/wav'
-		};
 
-		s3.upload(params, function(err,data) {
-			console.log("uploading!");
-			console.log(err,data);
+
+	return new Promise((resolve, reject) => {
+		fs.readFile(file, function(err,data) {
+			if(err !== null) {
+				reject(err);
+			}
+			var params = {
+				Bucket: 'audioclip-bot', 
+				Key: 'test.wav', 
+				Body: data,
+				ACL: 'public-read',
+				ContentType: 'audio/wav'
+			};
+
+			s3.upload(params, function(err,data) {
+				console.log("Upload completed!");
+				if(err === null) {
+					resolve(data.Location);
+				} else {
+					reject(err);
+				}
+			});
 		});
 	});
 }
